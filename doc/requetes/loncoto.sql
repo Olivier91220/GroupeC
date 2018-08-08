56404ffc8d1a36359fffd5b10038b195c4997aea
zetoun <olivier.goguelat@gmail.com>
2018-08-08 17:17:09 +0200

    MAJ diag ER + base loconto Myadmin tests requetes

---
+++ doc/requetes/loncoto.sql
@@ -0,0 +1,319 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 08 août 2018 à 17:08
+-- Version du serveur :  10.1.33-MariaDB
+-- Version de PHP :  7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `loncoto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `appartenir`
+--
+
+CREATE TABLE `appartenir` (
+  `groupe_id` int(11) NOT NULL,
+  `intervenant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `appartenir`
+--
+
+INSERT INTO `appartenir` (`groupe_id`, `intervenant_id`) VALUES
+(1, 1),
+(2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `id_sousFamille` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(50) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `nom` int(11) NOT NULL,
+  `prenom` int(11) NOT NULL,
+  `adresse` varchar(150) COLLATE utf8mb4_bin NOT NULL,
+  `code_postal` int(5) NOT NULL,
+  `ville` int(40) NOT NULL,
+  `tel` int(14) NOT NULL,
+  `email` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `famille`
+--
+
+CREATE TABLE `famille` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(40) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+CREATE TABLE `groupe` (
+  `id` int(11) NOT NULL,
+  `intervenant_id` int(11) NOT NULL,
+  `nom_groupe` varchar(30) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `intervenant_id`, `nom_groupe`) VALUES
+(1, 1, 'region ile de france'),
+(2, 1, 'region Haut de France');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervenant`
+--
+
+CREATE TABLE `intervenant` (
+  `id` int(11) NOT NULL,
+  `groupe_id` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `prenom` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `poste` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `tel` varchar(14) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(70) COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `intervenant`
+--
+
+INSERT INTO `intervenant` (`id`, `groupe_id`, `nom`, `prenom`, `poste`, `tel`, `email`) VALUES
+(1, 1, 'vaillant', 'michel', 'agent de maintenance', '0652415487', 'mv@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervention`
+--
+
+CREATE TABLE `intervention` (
+  `id` int(11) NOT NULL,
+  `materiel_id` int(11) NOT NULL,
+  `intervenant_id` int(11) NOT NULL,
+  `date_planification` date NOT NULL,
+  `type_intervention` varchar(10) COLLATE utf8mb4_bin NOT NULL DEFAULT 'curative',
+  `date_realisation` date NOT NULL,
+  `statut` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `commentaire` varchar(100) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+CREATE TABLE `materiel` (
+  `id` int(11) NOT NULL,
+  `salle_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `rapport_id` int(11) NOT NULL,
+  `num_serie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rapport`
+--
+
+CREATE TABLE `rapport` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `observation` varchar(100) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sous_famille_article`
+--
+
+CREATE TABLE `sous_famille_article` (
+  `id` int(11) NOT NULL,
+  `famille_id` int(11) NOT NULL,
+  `nom` varchar(40) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `appartenir`
+--
+ALTER TABLE `appartenir`
+  ADD PRIMARY KEY (`groupe_id`,`intervenant_id`),
+  ADD KEY `groupe` (`groupe_id`) USING BTREE,
+  ADD KEY `intervenant` (`intervenant_id`) USING BTREE;
+
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `famille`
+--
+ALTER TABLE `famille`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `groupe`
+--
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `intervenant`
+--
+ALTER TABLE `intervenant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `intervention`
+--
+ALTER TABLE `intervention`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_serie` (`num_serie`),
+  ADD UNIQUE KEY `num_serie_2` (`num_serie`);
+
+--
+-- Index pour la table `rapport`
+--
+ALTER TABLE `rapport`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `sous_famille_article`
+--
+ALTER TABLE `sous_famille_article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `famille`
+--
+ALTER TABLE `famille`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `groupe`
+--
+ALTER TABLE `groupe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `intervenant`
+--
+ALTER TABLE `intervenant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `intervention`
+--
+ALTER TABLE `intervention`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `rapport`
+--
+ALTER TABLE `rapport`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `sous_famille_article`
+--
+ALTER TABLE `sous_famille_article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `appartenir`
+--
+ALTER TABLE `appartenir`
+  ADD CONSTRAINT `Fk_groupe` FOREIGN KEY (`groupe_id`) REFERENCES `groupe` (`id`),
+  ADD CONSTRAINT `Fk_intervenant` FOREIGN KEY (`intervenant_id`) REFERENCES `intervenant` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
